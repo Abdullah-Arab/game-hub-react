9d1760224d3403a516b398beecf4c3d72b863033
Abdullah Arab <abdo.arab2014@gmail.com>
2023-10-11 13:19:39 +0200

    refactor: game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,25 +14,24 @@ const GameGrid = ({gameQuery}: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletones = [1,2,3,4,5,6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 4,
-        }}
-        spacing={6}
-        paddingY={3}
-      >
-        {isLoading &&  skeletones.map(skeletone => <GameCardContainer key={skeletone}><GameCardSkeletone ></GameCardSkeletone></GameCardContainer>)}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}><GameCard  game={game} /></GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+    columns={{
+      sm: 1,
+      md: 2,
+      lg: 3,
+      xl: 4,
+    }}
+    spacing={6}
+    paddingY={3}
+  >
+    {isLoading &&  skeletones.map(skeletone => <GameCardContainer key={skeletone}><GameCardSkeletone ></GameCardSkeletone></GameCardContainer>)}
+    {data.map((game) => (
+      <GameCardContainer key={game.id}><GameCard  game={game} /></GameCardContainer>
+    ))}
+  </SimpleGrid>
   );
 };
 
